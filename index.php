a7d16b02c48d8303a82bb95d085ecbb576fad970
TiagoBezerra285 <tnthiagolima88@gmail.com>
2017-05-04 08:59:11 -0300

    view
    view

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Document</title>
+	 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"/>
+</head>
+<body>
+	<div class="container">
+		<div class="jumbotron">
+			<h1>Página inicial</h1>
+			<a href="http://localhost/phpmvc/view/login.php">Login</a>
+		</div>
+	</div>
+</body>
+</html>
---
+++ view.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en"> 
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
+</head>
+<body>
+	<div class="container">
+		<div class="jumbotron">
+			<h1>Exibindo os dados da view</h1>
+		</div>
+		<dir class="panel panel-defaut">
+			<div clss="panel-body">
+				<h2>Dados: <?php echo $dados; ?></h2>
+			</div>
+		</dir>
+	</div>
+</body>
+</html>
